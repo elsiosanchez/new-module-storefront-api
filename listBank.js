d5412a4be10edd0fe5c4e4e5085984f918fa8db2
Elsio15 <elsiosanches@gmail.com>
2020-09-01 11:16:37 -0400

    add list the bank

---
+++ listBank.js
@@ -0,0 +1,91 @@
+export default [
+  {
+    code: '0102',
+    label: 'Banco de Venezuela',
+    has_p2p: true
+  }, {
+    code: '0104',
+    label: 'Venezolano de Crédito',
+    has_p2p: true
+  }, {
+    code: '0105',
+    label: 'Mercantil',
+    has_p2p: true
+  }, {
+    code: '0108',
+    label: 'Provincial',
+    has_p2p: true
+  }, {
+    code: '0114',
+    label: 'Bancaribe',
+    has_p2p: true
+  }, {
+    code: '0115',
+    label: 'Exterior',
+    has_p2p: true
+  }, {
+    code: '0116',
+    label: 'Occidental de Descuento',
+    has_p2p: true
+  }, {
+    code: '0128',
+    label: 'Banco Caroní',
+    has_p2p: true
+  }, {
+    code: '0134',
+    label: 'Banesco',
+    has_p2p: true
+  }, {
+    code: '0138',
+    label: 'Banco Plaza',
+    has_p2p: true
+  }, {
+    code: '0151',
+    label: 'BFC Banco Fondo Común',
+    has_p2p: true
+  }, {
+    code: '0156',
+    label: '100% Banco',
+    has_p2p: true
+  }, {
+    code: '0157',
+    label: 'Del Sur',
+    has_p2p: true
+  }, {
+    code: '0163',
+    label: 'Banco del Tesoro',
+    has_p2p: true
+  }, {
+    code: '0166',
+    label: 'Banco Agrícola de Venezuela',
+    has_p2p: true
+  }, {
+    code: '0168',
+    label: 'Bancrecer',
+    has_p2p: true
+  }, {
+    code: '0169',
+    label: 'Mi Banco',
+    has_p2p: true
+  }, {
+    code: '0172',
+    label: 'Bancamiga',
+    has_p2p: true
+  }, {
+    code: '0174',
+    label: 'Banplus',
+    has_p2p: true
+  }, {
+    code: '0175',
+    label: 'Bicentenario del Pueblo',
+    has_p2p: true
+  }, {
+    code: '0177',
+    label: 'Banfanb',
+    has_p2p: true
+  }, {
+    code: '0191',
+    label: 'BNC Nacional de Crédito',
+    has_p2p: true
+  }
+]
